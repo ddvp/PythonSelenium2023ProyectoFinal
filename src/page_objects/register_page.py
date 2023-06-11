f85d458cbc867790b93c3085c67e628a7759ca2c
ddvp <ddvp0188@gmail.com>
2023-06-11 13:48:06 -0600

    Project update

---
+++ src/page_objects/register_page.py
@@ -0,0 +1,22 @@
+import logging
+
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from src.page_objects.base_page import BasePage
+
+
+class RegisterPage(BasePage):
+    def __init__(self, driver: WebDriver, wait_driver: WebDriverWait):
+        super(RegisterPage, self).__init__(driver, wait_driver)
+
+    def register_account(self, firstname: str, lastname: str, email: str, telephone: str, pswd: str, pswdConf: str):
+        logging.info(f"Enter first name {firstname} last name {lastname} email {email} telephone {telephone} pswd {pswd} pswdConf {pswdConf}")
+        self.element("firstnm_input").wait_clickable().send_keys(firstname)
+        self.element("lastnm_input").wait_clickable().send_keys(lastname)
+        self.element("email_input").wait_visible().send_keys(email)
+        self.element("telephone_input").wait_visible().send_keys(telephone)
+        self.element("pswd_input").wait_visible().send_keys(pswd)
+        self.element("pswdConf_input").wait_visible().send_keys(pswdConf)
+        self.element("agree_box").wait_visible().click()
+        self.element("continue_btn").wait_visible().click()
