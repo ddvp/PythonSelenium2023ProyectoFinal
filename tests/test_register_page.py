50db6a8b46ddfe83cc85f829eb0d3bd121c63579
ddvp <ddvp0188@gmail.com>
2023-06-11 16:14:02 -0600

    Project update

--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -1,10 +1,7 @@
-import time
-
 from src.page_objects.register_page import RegisterPage
-from src.page_objects.login_page import LoginPage
 
 
 def test_valid_register(web_drivers):
     register_page = RegisterPage(*web_drivers)
     register_page.open()
-    register_page.loggin("Juana", "Martinez","test@test.com","3311098733","test1234","test1234")
+    register_page.register_account("Juan", "Martinez", "juan@test.com", "3311098733", "test1234", "test1234")
