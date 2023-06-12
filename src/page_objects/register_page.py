9b3dd82698f9501ad4fed3a49bdb23df407fc560
ddvp <ddvp0188@gmail.com>
2023-06-11 20:10:29 -0600

    Project update

--- src/page_objects/register_page.py
+++ src/page_objects/register_page.py
@@ -10,13 +10,13 @@ class RegisterPage(BasePage):
     def __init__(self, driver: WebDriver, wait_driver: WebDriverWait):
         super(RegisterPage, self).__init__(driver, wait_driver)
 
-    def register_account(self, firstname: str, lastname: str, email: str, telephone: str, pswd: str, pswdConf: str):
-        self.element("firstnm_input").wait_clickable().send_keys(firstname)
-        self.element("lastnm_input").wait_clickable().send_keys(lastname)
-        self.element("email_input").wait_visible().send_keys(email)
-        self.element("telephone_input").wait_visible().send_keys(telephone)
-        self.element("pswd_input").wait_visible().send_keys(pswd)
-        self.element("pswdConf_input").wait_visible().send_keys(pswdConf)
-        self.element("agree_box").wait_visible().click()
-        self.element("continue_btn").wait_visible().click()
+    def register_account(self, first_name: str, last_name: str, email: str, telephone: str, pswd: str, pswdConf: str):
+        self.element("first_name_input").wait_clickable().send_keys(first_name)
+        self.element("last_name_input").wait_clickable().send_keys(last_name)
+        self.element("email_input").wait_clickable().send_keys(email)
+        self.element("telephone_input").wait_clickable().send_keys(telephone)
+        self.element("pswd_input").wait_clickable().send_keys(pswd)
+        self.element("pswdConf_input").wait_clickable().send_keys(pswdConf)
+        self.element("agree_box").wait_clickable().click()
+        self.element("continue_btn").wait_clickable().click()
 
--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -1,7 +1,7 @@
 from src.page_objects.register_page import RegisterPage
 
 
-def test_valid_register(web_drivers):
+def test_register(web_drivers):
     register_page = RegisterPage(*web_drivers)
     register_page.open()
     register_page.register_account("Juan", "Martinez", "juan@test.com", "3311098733", "test1234", "test1234")
